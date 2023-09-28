59adb638a33d9aca7933feedee094b956c4a8762
Ian Knighton <iknighton@deseretdigital.com>
2023-09-28 16:35:41 -0600

    Fixes Variable

--- dist/index.js
+++ dist/index.js
@@ -10684,7 +10684,7 @@ try {
     console.log("Story Name: ".concat(storyTitle));
     console.log("Description: ".concat(storyDescription));
     console.log("Teams: ".concat(assignedTeams));
-    var shortcut = new client_1.ShortcutClient('6515fdc0-c156-4c81-ad38-d1edafab4aac');
+    var shortcut = new client_1.ShortcutClient(apiKey);
     shortcut.getCurrentMemberInfo().then(function (response) { return console.log(response === null || response === void 0 ? void 0 : response.data); });
     shortcut.listProjects().then(function (response) { return console.log(response === null || response === void 0 ? void 0 : response.data); });
     // Get the JSON webhook payload for the event that triggered the workflow
